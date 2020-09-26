6b950784e0d386a7ee771c59ed1151659a9765f2
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-09-26 16:04:04 +0000

    Deploy turkaytunc/digital-clock to github.com/turkaytunc/digital-clock.git:gh-pages

---
+++ precache-manifest.0e951ef0333449b3f1a6d0de24ca2930.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f27a2209f7309b4f6ee802647c35589d",
+    "url": "/digital-clock/index.html"
+  },
+  {
+    "revision": "18d7a09b97b3b839656a",
+    "url": "/digital-clock/static/css/main.b58aed19.chunk.css"
+  },
+  {
+    "revision": "3560bbb995b220b51030",
+    "url": "/digital-clock/static/js/2.2e009a0d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/digital-clock/static/js/2.2e009a0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18d7a09b97b3b839656a",
+    "url": "/digital-clock/static/js/main.62abb5c5.chunk.js"
+  },
+  {
+    "revision": "0caff2d2ec2152c590ca",
+    "url": "/digital-clock/static/js/runtime-main.e71a8198.js"
+  }
+]);
